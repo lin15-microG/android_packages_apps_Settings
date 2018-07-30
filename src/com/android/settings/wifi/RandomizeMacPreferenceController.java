jonrichards/platform_packages_apps_Settings

add toggle for associated MAC randomization
Change-Id: I053ac25f0388f1ed686510c9ce099db2b17c9ba2

src/com/android/settings/wifi/RandomizeMacPreferenceController.java
@@ -1,0 +1,42 @@
+package com.android.settings.wifi;
+
+import android.content.Context;
+import android.os.SystemProperties;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+
+import com.android.settings.core.PreferenceController;
+
+public class RandomizeMacPreferenceController extends PreferenceController
+        implements Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_RANDOMIZE_MAC = "randomize_mac";
+
+    private SwitchPreference mRandomizeMacPreference;
+
+    public RandomizeMacPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        boolean value = SystemProperties.getBoolean("persist.privacy.randomize_mac", true);
+        ((SwitchPreference) preference).setChecked(value);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        SystemProperties.set("persist.privacy.randomize_mac", (Boolean) newValue ? "1" : "0");
+        return true;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return SystemProperties.getBoolean("ro.randomize_mac", false);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_RANDOMIZE_MAC;
+    }
+}

lin15-microG/android_packages_apps_Settings

Change-Id: I47a92fd05ef06d6793c56f4adb8d781f4f8ab47d

src/com/android/settings/wifi/RandomizeMacPreferenceController.java
@@ -1,0 +1,43 @@
+package com.android.settings.wifi;
+
+import android.content.Context;
+import android.os.SystemProperties;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+public class RandomizeMacPreferenceController extends AbstractPreferenceController
+        implements PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_RANDOMIZE_MAC = "randomize_mac";
+
+    private SwitchPreference mRandomizeMacPreference;
+
+    public RandomizeMacPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        boolean value = SystemProperties.getBoolean("persist.privacy.randomize_mac", true);
+        ((SwitchPreference) preference).setChecked(value);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        SystemProperties.set("persist.privacy.randomize_mac", (Boolean) newValue ? "1" : "0");
+        return true;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return SystemProperties.getBoolean("ro.randomize_mac", false);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_RANDOMIZE_MAC;
+    }
+}