a404d081bfffe02188571b559a60e755cd022036
MSe1969 <mse1969@posteo.de>
2019-06-14 21:15:47 +0200

    Revert "Add microg to Setting"
    This reverts commit 2765891851977cf6d01d228bdc5eb22cc0c22cf4.
    
    microG GMS Copr 0.2.7.x now brings its own intent to appear in
    android's Settings menu, hence this one is a duplicate and not
    needed any longer.

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -27,7 +27,6 @@ import com.android.settings.IccLockSettings;
 import com.android.settings.MasterClear;
 import com.android.settings.PrivacySettings;
 import com.android.settings.SecuritySettings;
-import com.android.settings.MicroGSettings;
 import com.android.settings.Settings;
 import com.android.settings.TestingSettings;
 import com.android.settings.TetherSettings;
@@ -253,9 +252,7 @@ public class SettingsGateway {
             WebViewAppPicker.class.getName(),
             LockscreenDashboardFragment.class.getName(),
             BluetoothDeviceDetailsFragment.class.getName(),
-            DataUsageList.class.getName(),
-            MicroGSettings.class.getName()
-
+            DataUsageList.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
